d26a657c4e28ce98b17ff49ed7d2deaeb28a87e7
John Ravi <jostache@gmail.com>
2014-11-17 19:54:26 -0500

    nov 17

---
+++ facedetection/CMakeFiles/webcamtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/webcamtest.dir/webcamtest.cpp.o"
+  "webcamtest.pdb"
+  "webcamtest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/webcamtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
